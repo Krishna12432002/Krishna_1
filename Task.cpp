7f7110427b5796ad6e93d89717e605662e6ca929
Krishna12432002 <skkrishnapandirlapalli@gmail.com>
2024-11-25 16:35:40 +0530

    Code get changes

--- Task.cpp
+++ Task.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
-
+class hello
+{
+    public:
+  void display()
+  {
+    cout<<"display funcion "<<endl;
+  }
+};
 int main(){
     cout<<"Hello"<<endl;
+    hello h;
+    h.display();
 }
